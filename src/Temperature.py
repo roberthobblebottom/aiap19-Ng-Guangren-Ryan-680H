52090efd4523a5c946cad0736b17ee4ad76e95a3
themissingCRAM <pyroclastic@protonmail.com>
2024-12-06 20:54:42 +0800

    Temperature.py codes and documentations

--- src/Temperature.py
+++ src/Temperature.py
@@ -1,12 +1,6 @@
-# import marimo as mo
 import polars as pl
 
-import missingno
-
-import numpy as np
-from missforest import MissForest
-
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 
 from sklearn.ensemble import RandomForestRegressor
 
@@ -14,22 +8,20 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-
-
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
-import os
+from sklearn.metrics import (
+    mean_absolute_error,
+    root_mean_squared_error,
+    r2_score,
+)
 
 import Utils
-
-if not os.path.exists("images"):
-    os.mkdir("images")
+from scipy.stats import randint
 
 
 class Temperature:
     def __init__(self, df: pl.DataFrame) -> None:
-        self.kFold = KFold(n_splits=5)
 
+        # I failed in this one
         # if df.schema != pl.Schema(
         #     {
         #         "location": pl.String,
@@ -101,6 +93,8 @@ class Temperature:
             )
             .unique()  # remove duplicates
         )
+
+        # Some guards to prevent incorrect inputs or formats from being trained and tested
         if df.shape[0] == 0:
             raise Exception("There are no rows in the input Polars DataFrame.")
         df = df.filter(pl.col("temperature_celsius").is_not_nan())
@@ -171,25 +165,94 @@ class Temperature:
                 ),  # MissForest was not working because it kept detecting temperature
                 # even though I removed it from features.
                 # replaced with Simpleimputer for speed and availability
-                ("randomForestRegressor", RandomForestRegressor()),
+                (
+                    "randomForestRegressor",
+                    RandomForestRegressor(
+                        n_jobs=10,
+                        random_state=0,
+                    ),
+                ),
             ]
         )
 
     def train(self):
+        """
+        3 way hold out with cross validation it is used because
+        unlike 2 way hold out, the test error less depended on samples use
+        and may overestimate test error
+
+        these split is used in such a way that data leakage isn't present.
+        Data Leakage will give a overestimated test error.
+        """
         x_train, x_test, y_train, y_test = train_test_split(
             self.x, self.y, test_size=0.1
         )
 
-        # # Hyperparam tuning with 5 fold LOOCV
-        for x_indices, y_indices in self.kFold.split(x_train, x_test):
+        # These test splits are for evaluation later
+        self.x_test = x_test
+        self.y_test = y_test
 
-            self.pipeline.fit(x_train, y_train)
-            self.pipeline.train()
+        """
+         For hyperparmater tuning. Faster than grid search or bayesian optimisation. 
+         includes 5 fold cross validation:
+        """
+        print("hyperparameter tuning")
+        param_dist = {
+            "randomForestRegressor__n_estimators": randint(10, 200),
+            "randomForestRegressor__max_depth": randint(1, 20),
+            "randomForestRegressor__min_samples_split": randint(2, 11),
+            "randomForestRegressor__min_samples_leaf": randint(1, 11),
+        }
+        rs = RandomizedSearchCV(
+            self.pipeline,
+            param_distributions=param_dist,
+            n_iter=10,  # TODO change to a bigger number
+            cv=5,
+            scoring="neg_mean_absolute_error",
+            n_jobs=10,
+        )
+        rs.fit(x_train, y_train.iloc[:, 0].ravel())
+        best_params = rs.best_params_
+        print("best_params", best_params)
+
+        print("training model")
+        self.pipeline.set_params(
+            {
+                "randomForestRegressor__n_estimators": best_params[
+                    "randomForestRegressor__n_estimators"
+                ],
+                "randomForestRegressor__max_depth": best_params[
+                    "randomForestRegressor__max_depth"
+                ],
+                "randomForestRegressor__min_samples_split": best_params[
+                    "randomForestRegressor__min_samples_split"
+                ],
+                "randomForestRegressor__min_samples_leaf": best_params[
+                    "randomForestRegressor__min_sample_leaf"
+                ],
+            }
+        )
+        self.pipeline.fit(x_train, y_train)
 
     def evaluate(self):
-        pass
+        """
+        Rationale for metrics used.
+        Mean Absolute Error: Rombust to outliers,Interpretability
+        Root Mean Squared Error: More interpretable than mean squared error, Less sensitive to large errors
+        r2: Proportion of variance explained, Scale indenpendce compare to the other regression metrics, ease of intepretability
+        """
+        # predictions = self.pipeline.predict(self.x_test)
+        prediction_probabilities = self.pipeline.predict_proba(self.x_test)
+        r2 = r2_score(self.y_test, prediction_probabilities)
+        mae = mean_absolute_error(self.y_test, prediction_probabilities)
+        rmse = root_mean_squared_error(self.y_test, prediction_probabilities)
+        print("r square score", r2)
+        print("mean absolute error", mae)
+        print("root mean squared error", rmse)
 
 
 if __name__ == "__main__":
     db = Utils.connect_sqlite("agri.db")
-    Temperature(db).train()
+    t = Temperature(db)
+    t.train()
+    t.evaluate()
